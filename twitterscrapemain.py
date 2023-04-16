446ec388bb98f9409f93a8b9be25834474e2ec9c
Mogith P N <113936190+Mogith-P-N@users.noreply.github.com>
2023-04-17 00:50:40 +0530

    Update twitterscrapemain.py

--- twitterscrapemain.py
+++ twitterscrapemain.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import snscrape.modules.twitter as sntwitter
 
 #page setup 
 st.set_page_config(page_title="Twitter scrape", page_icon="🐍", layout="wide")
@@ -7,3 +8,10 @@ st.title("Twitter scrape")
 
 #input account name or topic or hashtag to scrape data from twitter
 name= st.text_input("Scrape data by name or hastag", value="")
+
+data=[]
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:name').get_items()):
+   if i>100:
+        break
+   data.append([tweet.date, tweet.likeCount, tweet.sourceLabel, tweet.content])
+st.write(data)
